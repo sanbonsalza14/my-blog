9f36c3ad31a955c0d511dca371b182c3fe3fa2b4
804-03 <slamguy14@naver.com>
2025-09-15 14:34:24 +0900

    초기커밋

---
+++ src/Modal.jsx
@@ -0,0 +1,12 @@
+function Modal() {
+  return (
+    <div className='modal'>
+      {/* 상세페이지용 모달창 만들기 */}
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+export default Modal;
