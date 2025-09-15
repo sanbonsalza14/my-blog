241ae9f7d625b9e428dabc2e05cf012913c31a3d
804-03 <slamguy14@naver.com>
2025-09-15 15:40:54 +0900

    Map을 이용한 반복처리 수행

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import Modal from './Modal';
 
 function App() {
 
-  //데이터 바인딩
+  // 데이터 바인딩
   let post = '강남제육맛집';
 
   const [title, setTitle] = useState([
@@ -13,84 +13,83 @@ function App() {
     '자바스터디'
   ]);
 
-  const [createDate, setCreateDate] = useState([   // ← 오타 수정
+  const [createDate, setCreateDate] = useState([
     '2025-5-1',
     '2025-6-1',
-    '2025-7-1'
+    '2025-7-1',
   ]);
 
-  //좋아요 누름 숫자를 보관할 스테이트
+  // 좋아요 누름 숫자를 보관할 스테이트
   const [like, setLike] = useState([0, 0, 0]);
 
-  //좋아요 처리 함수 
-  function addLikes(num) {
-    let newLikes = [...like];
-    newLikes[num] += 1;
-    setLike(newLikes);
-  }
+  // 좋아요 처리 함수
+  // function addLikes(num){
+  //   setLike(like[num]+1)
+  // }
 
   function changeTitle() {
     const newTitle = [...title];
-    newTitle[0] = '여자코트추천';
+    newTitle[0] = '여자코트추천'; // ← 오타 수정
     setTitle(newTitle);
   }
 
-  //모달페이지가 보이게 /안보이게 자겁하기위한 스테이트
-  const [modal, setModal] = useState(true);
+  // 모달페이지가 보이게/안보이게 작업하기위한 스테이트
+  const [modal, setModal] = useState(false);
+
+  // 직전 선택한 인덱스를 저장할 스테이트
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  // 제목 클릭 시 모달 보이기 
+  function handleTitle(index) {
+    // 조건 : 같은 제목을 클릭하면 나타나고 사라짐 
+    // 다른 제목을 클릭하면 그냥 보여야 하고..
+    if (!modal) {
+      // 1. 현재 모달이 닫혀있으면 연다.
+      setModal(true);
+      setCurrentIndex(index);
+    } else if (currentIndex === index) {
+      // 2. 같은 타이틀이 선택된 경우
+      setModal(false);
+    } else {
+      setCurrentIndex(index);
+    }
+  }
 
   return (
     <div className='App'>
       <div className='black-bg'>
         React + Vite로 만드는 블로그
       </div>
-      {/* <h4 style={{ color: 'red', fontSize: '20px' }}>{post}</h4> */}
+      {/* <h4 style={{color: 'red', fontSize: '20px'}}>{post}</h4> */}
 
-      {/* {타이틀 정렬하기 } */}
+      {/* 타이틀 정렬하기 */}
       <button onClick={() => {
-        const sortedTitle = [...title].sort();
+        const sortedTitle = [...title].sort()
         setTitle(sortedTitle);
       }}>글 정렬하기</button>
 
       <div className='list'>
-        <div>
-          <h4 onClick={()=>{
-            setModal
-          }}>{title[0]}
-            <span onClick={() => {
-              const newLikes = [...like];
-              newLikes[0]++;
-              setLike(newLikes);
-            }}
-            >👍</span>{like[0]}
-            <button onClick={changeTitle}>변경</button>  {/* ← onClick 오타 수정 */}
-          </h4>
-          <p>작성일 : {createDate[0]}</p>
-        </div>
-        <div>
-          <h4>{title[1]}
-            <span onClick={() => {
-              const newLikes = [...like];
-              newLikes[1]++;
-              setLike(newLikes);
-            }}
-            >👍</span>{like[1]}
-          </h4>
-          <p>작성일 : {createDate[1]}</p>
-        </div>
-        <div>
-          <h4>{title[2]}
-            <span onClick={() => {
-              const newLikes = [...like];
-              newLikes[2]++;
-              setLike(newLikes);
-            }}
-            >👍</span>{like[2]}
-          </h4>
-          <p>작성일 : {createDate[2]}</p>
-        </div>
+        {title.map((item, index) => {
+          return (
+            <div key={index}>
+              <h4 onClick={() => {
+                setModal(!modal)
+              }}>{title[index]}
+                <span onClick={() => {
+                  const newLikes = [...like]
+                  newLikes[index]++
+                  setLike(newLikes)
+                }}>👍
+                </span>{like[index]}
+              </h4>
+              <p>작성일 : {createDate[index]}</p>
+            </div>
+          )
+        })}
+
+        {/* 상세페이지 나타날 곳 */}
+        {modal ? <Modal /> : null}
       </div>
-       {/* 상세페이지 나타날 곳 */}
-      {Modal ? <Modal/>: null}
     </div>
   )
 }
